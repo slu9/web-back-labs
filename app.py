b65cf5deadbd16358149f99e66e9b2d8827b4a33
Софья Зырянова <s0niksluggard@yandex.ru>
2025-10-02 08:58:32 +0700

    Имя студента

--- app.py
+++ app.py
@@ -373,4 +373,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-        return render_template('example.html')
+        name = 'Зырянова Софья'
+        return render_template('example.html', name=name)
