378f3af3c322f8161a2f41b9a7c1822a810c184a
Софья Зырянова <s0niksluggard@yandex.ru>
2025-10-30 16:47:37 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -116,16 +116,23 @@ def tree():
 
     return render_template('lab4/tree.html', tree_count=tree_count)
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'sofya', 'password': '111'},
+    {'login': 'kate', 'password': '222'},
+]
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('lab4/login.html', authotized=False)
+        return render_template('lab4/login.html', authorized=False)
 
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('lab4/login.html', login=login, authorized=True)
     
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorized=False)
