a6e64557e7ddd8ff17516e4f76d378dcf541fc77
Софья Зырянова <s0niksluggard@yandex.ru>
2025-11-13 22:22:55 +0700

    Правки для SQLite

--- lab5.py
+++ lab5.py
@@ -46,14 +46,14 @@ def register():
 
     conn, cur = db_connect()
     
-    cur.execute("SELECT login FROM users WHERE login=%s;", (login, ))
+    cur.execute("SELECT login FROM users WHERE login=?;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html', 
                                error="Такой пользователь уже существует")
 
     password_hash = generate_password_hash(password)
-    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
+    cur.execute("INSERT INTO users (login, password) VALUES (?, ?);", (login, password_hash))
 
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
@@ -71,7 +71,7 @@ def login():
     
     conn, cur = db_connect()
 
-    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+    cur.execute("SELECT * FROM users WHERE login=?;", (login, ))
     user = cur.fetchone()
 
     if not user:
@@ -101,12 +101,12 @@ def create():
     
     conn, cur = db_connect()
 
-    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+    cur.execute("SELECT * FROM users WHERE login=?;", (login, ))
     user_id = cur.fetchone()["id"]
 
     cur.execute(
             "INSERT INTO articles(user_id, title, article_text) " \
-            "VALUES (%s, %s, %s);",
+            "VALUES (?, ?, ?);",
             (user_id, title, article_text)
         )
 
@@ -121,10 +121,10 @@ def list():
     
     conn, cur = db_connect()
 
-    cur.execute("SELECT id FROM users WHERE login=%s;", (login,))
+    cur.execute("SELECT id FROM users WHERE login=?;", (login,))
     user_id = cur.fetchone()["id"]
 
-    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (user_id,))
+    cur.execute("SELECT * FROM articles WHERE user_id=?;", (user_id,))
     articles = cur.fetchall()
 
     db_close(conn, cur)
