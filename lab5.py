021ce83cc724e9065a315fd4890ab7af23f09c4e
Софья Зырянова <s0niksluggard@yandex.ru>
2025-11-13 19:54:38 +0700

    Создание статьи

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -55,11 +55,6 @@ def list_articles():
     user = 'anonymous'
     return render_template('lab5/list.html', user=user)
 
-@lab5.route('/lab5/create')
-def create_article():
-    user = 'anonymous'
-    return render_template('lab5/create.html', user=user)
-
 @lab5.route('/lab5/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -88,3 +83,29 @@ def login():
     session['login'] = login
     db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login)
+
+@lab5.route('/lab5/create', methods = ['GET', 'POST'])
+def create():
+    login=session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    
+    if request.method == 'GET':
+        return render_template('lab5/create_article.html')
+    
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+    
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+    user_id = cur.fetchone()["id"]
+
+    cur.execute(
+            "INSERT INTO articles(user_id, title, article_text) " \
+            "VALUES (%s, %s, %s);",
+            (user_id, title, article_text)
+        )
+
+    db_close(conn, cur)
+    return redirect('/lab5')
