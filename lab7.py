9c48343e5ecae26aa6c285dbb4c1c694cfaca003
Зырянова Софья <s0niksluggard@yandex.ru>
2025-11-29 12:40:47 +0700

    Добавление нового фильма по REST

--- lab7.py
+++ lab7.py
@@ -65,3 +65,10 @@ def put_film(id):
     film = request.get_json()
     films[id] = film
     return films[id]
+
+@lab7.route('/lab7/rest-api/films', methods=['POST'])
+def add_film():
+    new_film = request.get_json()
+    films.append(new_film)
+    new_id = len(films) - 1
+    return {"id": new_id}, 201
