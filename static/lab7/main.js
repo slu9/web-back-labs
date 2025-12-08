0195d63911245cd99af664aa3eb561fe08c3477f
Зырянова Софья <s0niksluggard@yandex.ru>
2025-12-08 12:54:27 +0700

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -20,6 +20,7 @@ function addFilm() {
 }
 
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title-ru').value,
@@ -27,8 +28,8 @@ function sendFilm() {
         description: document.getElementById('description').value
     }
 
-    const url = `/lab7/rest-api/films/`;
-    const method = 'POST';
+    const url = `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST' : 'PUT';
 
     fetch(url, {
         method: method,
@@ -38,6 +39,21 @@ function sendFilm() {
     .then(function() {
         fillFilmList();
         hideModal();
+    });
+}
+
+function editFilm(id) {
+    fetch(`/lab7/rest-api/films/${id}`)
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (film) {
+        document.getElementById('id').value = id;
+        document.getElementById('title').value = film.title;
+        document.getElementById('title-ru').value = film.title_ru;
+        document.getElementById('year').value = film.year;
+        document.getElementById('description').value = film.description;
+        showModal();
     });
 }
 
@@ -74,6 +90,9 @@ function fillFilmList() {
 
                 let editButton = document.createElement('button');
                 editButton.innerText = 'редактировать';
+                editButton.onclick = function() {
+                    editFilm(i);
+                };
 
                 let delButton = document.createElement('button');
                 delButton.innerText = 'удалить';
