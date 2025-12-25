eeb3671cf24a14153a95740429e13abb5e1fc057
Зырянова Софья <s0niksluggard@yandex.ru>
2025-12-26 02:07:53 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask (__name__)
 
@@ -54,6 +55,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 visit_log = []
 
@@ -131,6 +133,7 @@ def index():
             <li><a href="/lab6">Шестая лабораторная работа</a></li>
             <li><a href="/lab7">Седьмая лабораторная работа</a></li>
             <li><a href="/lab8">Восьмая лабораторная работа</a></li>
+            <li><a href="/lab9">Девятая лабораторная работа</a></li>
         </ol>
     </main>
     <footer>
