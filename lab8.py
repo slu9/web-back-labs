dfbe988d1bbb29efe149913d1a4e0f19780519fa
Зырянова Софья <s0niksluggard@yandex.ru>
2025-12-26 00:22:55 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -2,13 +2,14 @@ from flask import Blueprint, render_template, request, redirect, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import db
 from db.models import users, articles
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab8 = Blueprint('lab8', __name__)
+from flask_login import current_user
 
 @lab8.route('/lab8/')
 def index():
-    username = 'anonymous'
+    username = current_user.login if current_user.is_authenticated else 'anonymous'
     return render_template('lab8/index.html', username=username)
 
 @lab8.route('/lab8/login', methods=['GET', 'POST'])
@@ -81,6 +82,12 @@ def register():
 @login_required
 def article_list():
     return "список статей"
+
+@lab8.route('/lab8/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8')
 
 @lab8.route('/lab8/create')
 def create():
